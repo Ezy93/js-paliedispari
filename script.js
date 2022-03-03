327495416429eadf7e6608c5ef20f274f10b2fd7
Ezy93 <giordani001@gmail.com>
2022-03-03 15:41:21 +0100

    aggiunti i  ;  per la felicità del sig. petricca

--- script.js
+++ script.js
@@ -12,52 +12,52 @@
 /* 1) L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.  //!check */
 
 let userGambling = prompt("scrivi la tua scelta tra pari o dispari:");
-console.log(userGambling)
+console.log(userGambling);
 let userNumber = parseInt(prompt("scegli un numero compreso tra 1 e 5:")); 
-console.log(userNumber)
+console.log(userNumber);
 
 /* 2) Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). //!check */
 function CPURandomNumber(){
     let randomNumber = Math.floor(Math.random()*5) + 1;
-    return randomNumber
+    return randomNumber;
 }
 
 /* 4) Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) //!check */
 function oddOEven(number){
     if(number % 2 == 0){
-        return true
+        return true;
     }else{
-        return false
+        return false;
     }
 }
 
-let random = CPURandomNumber()
-console.log( random)
+let random = CPURandomNumber();
+console.log( random);
 
 /* 3) Sommiamo i due numeri //! check*/
-let sum = userNumber + random
-console.log(sum)
+let sum = userNumber + random;
+console.log(sum);
 
 /* 5) Dichiariamo chi ha vinto. */ 
 
 if(oddOEven(sum)){
     
-    let controlVariable = "pari"
+    let controlVariable = "pari";
     
     if(userGambling === controlVariable){
-        console.log("hai vinto la somma è pari") //!check
+        console.log("hai vinto la somma è pari"); //!check
     }else{
-        console.log("hai perso la somma è pari") //!check
+        console.log("hai perso la somma è pari"); //!check
     }
     
 }else{
     
-    let controlVariable = "dispari"
+    let controlVariable = "dispari";
     
     if(userGambling === controlVariable){
-        console.log("hai vinto la somma è dipari") //!check
+        console.log("hai vinto la somma è dipari"); //!check
     }else{
-        console.log("hai perso la somma è dispari")  //! check
+        console.log("hai perso la somma è dispari");  //! check
     }
     
 }
